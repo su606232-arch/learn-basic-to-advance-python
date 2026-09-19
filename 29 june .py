34b2efed0b5c1a03239e1ba0e5dd3e94c985319e
saurabh upadhyay <su606232@gmail.com>
2026-09-19 13:14:44 +0530

    Add files via upload

---
+++ 29 june .py
@@ -0,0 +1,16 @@
+ # decision
+ # if else 2 match
+ # control statment
+ # loop - repeat a set of statments upto specific num of times or until any give conditon is not staisfied
+# two types of loop 1 while loop 2 for 
+# inpute name and display it 5 times
+#na = input (" enter names ")
+#c=0
+#while c<=10:
+  #  print (na)
+   # c=+1
+x=1
+
+while x<=30:
+    print (x)
+    x=+3
