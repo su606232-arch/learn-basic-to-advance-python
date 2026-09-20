9c2884f72e0a88ebc47aa7d97228274b0c6428d2
saurabh upadhyay <su606232@gmail.com>
2026-09-20 13:13:01 +0530

    Add files via upload

---
+++ 30june .py
@@ -0,0 +1,16 @@
+#while loop
+# 1 initialize the var on which loop depends before loop
+# 2 codtion 3 updatiion 
+
+
+
+
+# continue
+c=1
+while c<=3:
+    
+    print ("hello")
+    c=c+1
+    continue
+    print ("dego")
+print ( 'saurabh')
---
+++ 30june 2.py
@@ -0,0 +1,7 @@
+c=1
+while c<=5:
+    print (" hello ")
+    c = c+1
+    if c==4:
+        continue
+    print ("bye")
