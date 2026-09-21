b5d1851af46da022b59eb9b3155aed1cc354bc9d
saurabh upadhyay <su606232@gmail.com>
2026-09-21 10:34:40 +0530

    Add files via upload

---
+++ 30june 3.py
@@ -0,0 +1,20 @@
+# break
+c = 1
+while  c<=5:
+    print ( " hello ")
+    c+=1
+    break
+print (" tq")
+
+print ("////////////////////////////")
+
+
+c = 1
+while  c<=5:
+    print ( " hello ")
+    c+=1
+    
+    if c==3:
+        break
+    print ("saM")
+print (" tq")
